6a8c201e7de38db8d75d6a6d8c299f67a63f678e
brunozzz <16675056030>
2021-04-14 15:45:35 +0800

    encrypt

--- src/main/resources/static/assets/login.js
+++ src/main/resources/static/assets/login.js
@@ -9,8 +9,19 @@
    * @returns {string} 加密后的密码
    */
   function encrypt(passwd) {
-    const reversedPasswd = passwd.split('').reverse().join('');
-    return reversedPasswd;
+  let encrypt = "";
+      for (let i = 0; i < passwd.length; i++) {
+        let char = passwd[i];
+        if (char.toUpperCase(passwd[i])) {
+          let ch = String.fromCharCode((char.charCodeAt(0) + 3 - 65) % 26 + 65);
+          encrypt += ch;
+        } else if (char.toLowerCase(passwd[i])) {
+          let ch = String.fromCharCode((char.charCodeAt(0) + 3 - 97) % 26 + 97);
+          encrypt += ch;
+        } else
+         encrypt += passwd[i];
+      }
+    return encrypt;
   }
 
   /**
