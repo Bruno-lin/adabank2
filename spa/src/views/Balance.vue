485726a86621cdca861cec6528a0c787d507eba7
16675056030 <16675056030>
2021-04-18 14:33:35 +0800

    SPA

--- spa/src/views/Balance.vue
+++ spa/src/views/Balance.vue
@@ -1,5 +1,6 @@
 <template>
   <Card v-bind:image="require('../assets/account.png')">
+    <p>您好，{{ username }}！</p>
     <p>您当前余额为1234.56元</p>
   </Card>
 </template>
@@ -9,11 +10,26 @@ import Card from "../components/Card.vue";
 
 export default {
   name: "Balance",
-  // data(){
-  //   return{
-  //
-  //   };
-  // },
+  data() {
+    return {
+      username: this.getName(),
+    };
+  },
+  methods: {
+    getName() {
+      fetch("/api/name?session_id=" + document.cookie, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((response) => response.json())
+          .then((data) => {
+            if (data.status === "good") {
+              this.username = data.name
+            }
+          })
+    }
+  },
   components: {
     Card,
   }
